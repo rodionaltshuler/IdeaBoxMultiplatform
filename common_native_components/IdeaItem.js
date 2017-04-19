9f59f54c3fb2dbc9e98d60329bfd6ab17ff815a1
Rodion Altshuler <lohika.rodion@98point6.com>
2017-04-19 09:23:19 +0300

    Add upvotes and downvotes count+buttons to idea item

--- common_native_components/IdeaItem.js
+++ common_native_components/IdeaItem.js
@@ -1,19 +1,44 @@
 import React from "react";
-import {View, TouchableOpacity, ListView, Text, StyleSheet, StatusBar} from 'react-native';
+import {View, Image, TouchableOpacity, ListView, Text, StyleSheet, StatusBar} from 'react-native';
+
+const thumbUp = require('../images/ic_thumb_up/ic_thumb_up.png');
+const thumbDown = require('../images/ic_thumb_down/ic_thumb_down.png');
 
 class IdeaItem extends React.Component {
 
     constructor(props, context) {
         super(props, context);
+        this.getUpvotesCount = this.getUpvotesCount.bind(this);
+        this.getDownvotesCount = this.getDownvotesCount.bind(this);
+    }
+
+    getUpvotesCount() {
+        return this.props.idea.upvotes ? Object.keys(this.props.idea.upvotes).length : 0;
+    }
+
+    getDownvotesCount() {
+        return this.props.idea.downvotes ? Object.keys(this.props.idea.downvotes).length : 0;
     }
 
     render() {
         return (
             <View
-                style={{flex:1, height:56, backgroundColor: '#ffffff', alignItems:'stretch', justifyContent:'center'}}>
+                style={{
+                    flex: 1,
+                    height: 64,
+                    backgroundColor: '#ffffff',
+                    alignItems: 'stretch',
+                    justifyContent: 'center'
+                }}>
                 <TouchableOpacity>
                     <Text style={styles.itemtext}>{this.props.idea.title}</Text>
                 </TouchableOpacity>
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+                    <Image style={styles.actionIcon} source={thumbUp}/>
+                    <Text style={styles.smallText}>{this.getUpvotesCount()}</Text>
+                    <Image style={styles.actionIcon} source={thumbDown}/>
+                    <Text style={styles.smallText}>{this.getDownvotesCount()}</Text>
+                </View>
             </View>
         );
     }
@@ -28,6 +53,12 @@ const styles = StyleSheet.create({
     itemtext: {
         fontSize: 20,
         textAlign: 'center'
+    },
+    actionIcon: {
+        margin: 4
+    },
+    smallText: {
+        margin: 4
     }
 });
 
